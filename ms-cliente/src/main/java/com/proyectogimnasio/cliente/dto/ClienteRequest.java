6619f5e7a04a206c7848a088ffbe016f2a6b2840
krist41 <kristine.spl@proton.me>
2026-06-08 09:57:53 -0400

    ClienteResponse listo, agregado datos del modelo y imports

--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/ClienteRequest.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/ClienteRequest.java
@@ -1,4 +1,19 @@
 package com.proyectogimnasio.cliente.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class ClienteRequest {
+    @NotBlank(message = "El nombre no debe quedar en blanco")
+    private String nombres;
+    @NotBlank(message = "El apellido no debe de quedar en blanco")
+    private String apellidos;
+    @NotBlank(message = "El run es obligatorio")
+    private String run;
+    @NotNull(message = "El correo es Obligatorio")
+    private String correo;
+    @NotNull(message = "Debe ingresar el id del plan")
+    private Long idPlan;
 }
--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/ClienteResponse.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/dto/ClienteResponse.java
@@ -1,4 +1,19 @@
 package com.proyectogimnasio.cliente.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ClienteResponse {
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private String run;
+    private String correo;
+    private Long idPlan;
 }
