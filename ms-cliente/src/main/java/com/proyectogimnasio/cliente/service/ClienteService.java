a4f2cb9252b30675acaed3f106c398d037da35af
krist41 <kristine.spl@proton.me>
2026-06-08 10:21:38 -0400

    Agregado los imports en ClienteService, tambien el repositorio y el planesclient, creado un maptoresponse en la capa de servicio

--- ms-cliente/src/main/java/com/proyectogimnasio/cliente/service/ClienteService.java
+++ ms-cliente/src/main/java/com/proyectogimnasio/cliente/service/ClienteService.java
@@ -1,9 +1,38 @@
 package com.proyectogimnasio.cliente.service;
 
+import com.proyectogimnasio.cliente.client.PlanesClient;
+import com.proyectogimnasio.cliente.dto.ClienteRequest;
+import com.proyectogimnasio.cliente.dto.ClienteResponse;
+import com.proyectogimnasio.cliente.model.Cliente;
+import com.proyectogimnasio.cliente.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ClienteService {
+    private final ClienteRepository repo;
+    private final PlanesClient client;
+
+
+//mapToResponse sirve para que todo quede ordenado o mapeado para que cuando se retorne, muestre los datos del cliente
+    private ClienteResponse mapToResponse(Cliente c, String token) {
+        log.info("Mapeando cliente",
+                keyValue("idCliente", c.getId())
+        );
+        var plan1 = client.getPlan(c.getIdPlan(), token);
+
+
+        return ClienteResponse.builder().id(c.getId()).nombres(c.getNombres()).
+                apellidos(c.getApellidos()).
+                run(c.getRun()).
+                correo(c.getCorreo()).
+                idPlan(c.getIdPlan()).
+                build();
+
+    }
 }
