a40fb44539516b88ac11f08df798df80f5a9060e
krist41 <kristine.spl@proton.me>
2026-06-10 08:52:47 -0400

    Creado denuevo el microservicio de planes

--- ms-planes/src/main/java/com/example/ms_planes/repository/PagosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms_planes.repository;
-
-import com.example.ms_planes.model.Pagos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagosRepository extends JpaRepository<Pagos, Long> {
-}
