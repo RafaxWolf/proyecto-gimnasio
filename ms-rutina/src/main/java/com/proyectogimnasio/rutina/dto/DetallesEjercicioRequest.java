d0acbd473ef263f135b037b0ad107a6b703f7483
krist41 <kristine.spl@proton.me>
2026-06-12 11:33:47 -0400

    Request de DetallesEjercicio, Ejercicio y Rutina terminado en ms-rutina

--- ms-rutina/src/main/java/com/proyectogimnasio/rutina/dto/DetallesEjercicioRequest.java
+++ ms-rutina/src/main/java/com/proyectogimnasio/rutina/dto/DetallesEjercicioRequest.java
@@ -1,4 +1,21 @@
 package com.proyectogimnasio.rutina.dto;
 
+import com.proyectogimnasio.rutina.model.Ejercicio;
+import com.proyectogimnasio.rutina.model.Rutina;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class DetallesEjercicioRequest {
+    private Ejercicio ejercicio;
+    private Rutina rutina;
+
+
+    @NotNull
+    private Integer numeroEjercicios;
+    @NotBlank
+    private String duracionRutina;
+    @NotBlank
+    private String tiempoDescanso;
 }
